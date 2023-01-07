ef7658d2ceda6b8e8803ccde27c66a44ee83c54b
AlanPerezD <52674692+AlanPerezD@users.noreply.github.com>
2023-01-07 18:19:47 -0300

    final commit

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { SubHeading } from '../../components';
-import { images } from '../../components';
+import { images } from '../../constants';
 
 const FindUs = () => (
   <div className='app__bg app__wrapper section__padding' id='contact'>
@@ -10,8 +10,11 @@ const FindUs = () => (
       <h1 className='headtext__cormorant' style={{ marginBottom: '3rem'}}>Find Us</h1>
       <div className='app__wrapper-content'>
         <p className='p__opensans'>Lane Ends Bungalow, WhatCroft Hall Lane, Rudhear</p>
+        <p className='p__cormorant' style={{ color: '#DCCA87', margin: '2rem 0'}}>Opening Hours</p>
+        <p className='p__opensans'>Mon - Fri: 10:00 am - 02:00 am</p>
+        <p className='p__opensans'>Sat - Sun: 10:00 am - 03:00 am</p>
       </div>
-    
+      <button className='custom__button' style={{marginTop: '2rem'}}>Visit Us</button>
     </div>
     
     <div className='app__wrapper_img'>
